cfca3da727510d4f62b23930a69fd35ea2b354f1
fahadvillan6 <62593931+fahadvillan6@users.noreply.github.com>
2022-12-04 17:47:46 +0530

    PostCreated

--- posts/src/Controllers/PostController.ts
+++ posts/src/Controllers/PostController.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
-import { createPost as Create } from "../Models/postModel";
+import PostModel, { createPost as Create } from "../Models/postModel";
 
 export const createPost = async (req: Request, res: Response) => {
   console.log(req.body, "ldkd");
   const { caption, image } = req.body;
   const { id } = req.body.user;
-  const Post = {
+  const Post = new PostModel({
     caption,
     image,
     userid: id,
-  };
- const response = await Create(Post);
-  
+  })
+    .save()
+    .then((data) => {
+      res.status(201).send({ success: true, data: data });
+    });
 };
